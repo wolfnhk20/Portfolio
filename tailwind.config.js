dd647e0f92dda3fa1a9e853023ff3bcc409cd88b
Ayush <ayushkulal21@gmail.com>
2023-08-30 13:26:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)"
+        ]
+      },
+    },
+    plugins: [],
+  }
+}
